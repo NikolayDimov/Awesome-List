20d79d52b25e1619dc9a7e9de98904ce3d771e6f
Nikolay Dimov <114500017+NikolayDimov@users.noreply.github.com>
2024-06-03 00:41:43 +0300

    List - contentContainerStyle (last image scroll to the top)

--- components/List/List.jsx
+++ components/List/List.jsx
@@ -6,22 +6,32 @@ import Animated, {
   useAnimatedScrollHandler,
   useSharedValue,
 } from "react-native-reanimated";
+import { IMAGE_SIZE } from "../ListItem/ListItem.style";
+import { Dimensions } from "react-native";
 
+const SCREEN_H = Dimensions.get("screen").height;
 export function List() {
   const scrollY = useSharedValue(0);
   const scrollHandler = useAnimatedScrollHandler({
     onScroll: (e) => {
       scrollY.value = e.contentOffset.y;
-    }
+    },
   });
   return (
-    <Animated.ScrollView scrollEventThrottle={16} onScroll={scrollHandler}>
+    <Animated.ScrollView
+      contentContainerStyle={{
+        height: IMAGES.length * IMAGE_SIZE.MAX + (SCREEN_H - IMAGE_SIZE.MAX),
+      }}
+      scrollEventThrottle={16}
+      onScroll={scrollHandler}
+    >
       {IMAGES.map((image, i) => (
         <ListItem
           image={image}
           key={image.title + i}
           scrollY={scrollY}
-          index={i} />
+          index={i}
+        />
       ))}
     </Animated.ScrollView>
   );
