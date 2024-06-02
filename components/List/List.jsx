d4d60dac97d670af1ac4ff8c3889bf655ea66715
Nikolay Dimov <114500017+NikolayDimov@users.noreply.github.com>
2024-06-03 00:37:17 +0300

    List component - Animated.ScrollView

--- components/List/List.jsx
+++ components/List/List.jsx
@@ -2,12 +2,27 @@ import { ScrollView } from "react-native-gesture-handler";
 import { s } from "./List.style";
 import { IMAGES } from "../../constant";
 import { ListItem } from "../ListItem/ListItem";
+import Animated, {
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from "react-native-reanimated";
+
 export function List() {
+  const scrollY = useSharedValue(0);
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: (e) => {
+      scrollY.value = e.contentOffset.y;
+    }
+  });
   return (
-    <ScrollView>
-      {IMAGES.map((image) => (
-        <ListItem image={image} />
+    <Animated.ScrollView scrollEventThrottle={16} onScroll={scrollHandler}>
+      {IMAGES.map((image, i) => (
+        <ListItem
+          image={image}
+          key={image.title + i}
+          scrollY={scrollY}
+          index={i} />
       ))}
-    </ScrollView>
+    </Animated.ScrollView>
   );
 }
--- components/ListItem/ListItem.jsx
+++ components/ListItem/ListItem.jsx
@@ -1,5 +1,24 @@
-import { Image } from "react-native";
-import { s } from "./ListItem.style";
-export function ListItem({ image }) {
-  return <Image source={image.picture} style={s.image} />;
+import { IMAGE_SIZE, s } from "./ListItem.style";
+import Animated, {
+  Extrapolate,
+  interpolate,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+
+export function ListItem({ image, scrollY, index }) {
+  const imgAnimStyle = useAnimatedStyle(() => {
+    const height = interpolate(
+      scrollY.value,
+      [index * IMAGE_SIZE.MAX, index * IMAGE_SIZE.MAX - IMAGE_SIZE.MAX],
+      [IMAGE_SIZE.MAX, IMAGE_SIZE.MIN],
+      Extrapolate.CLAMP
+    );
+
+    return {
+      height,
+    };
+  });
+  return (
+    <Animated.Image source={image.picture} style={[s.image, imgAnimStyle]} />
+  );
 }
